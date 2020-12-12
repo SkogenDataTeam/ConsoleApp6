e7360017969fbd2682bbaff571f6f31483c1c9a4
Endre Romstad <erdne.datsmor@gmail.com>
2020-12-12 18:39:41 +0100

    forandringer

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -15,8 +15,21 @@ namespace ConsoleApp6
 
             string inn = "alle fugler små. de er";
 
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(inn);
+            Console.ResetColor();
+            Console.WriteLine("-", 10);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Splitter stringen etter tegnet '.'");
+
+
             int i = inn.IndexOf(".");
             string nyinn = inn.Substring(0, i);
+            Console.WriteLine(nyinn);
+            Console.ResetColor();
+            Console.WriteLine("-", 10);
+
             if (nyinn == "alle fugler små")
             { Console.WriteLine("yes"); }
             else
